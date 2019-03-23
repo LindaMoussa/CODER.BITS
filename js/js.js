e719df3acbe322c657b2605066324f3582c7793a
LindaMoussa <linda.moussa.95@gmail.com>
2019-03-23 20:58:10 +0200

    v3

---
+++ js/js.js
@@ -0,0 +1,37 @@
+var x1 = 1;
+var x2 = 1;
+var x3 = 1;
+var x4 = 1;
+
+window.setInterval(function () {
+    $('.logodesign h4').html(x1);
+
+    if (x1 != 795) {
+        x1++;
+    }
+}, 12);
+window.setInterval(function () {
+   $('.websites h4').html(x2);
+
+    if (x2 != 1459) {
+        x2++;
+    }
+
+    
+}, 8);
+window.setInterval(function () {
+     $('.wordpress h4').html(x3);
+
+    if (x3 != 359) {
+        x3++;
+    }
+    
+}, 20);
+window.setInterval(function () {
+      $('.happycustomers h4').html(x4);
+
+    if (x4 != 2459) {
+        x4++;
+    }
+    
+}, 5);
